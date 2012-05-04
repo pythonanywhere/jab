041f5e21b24674dc0c3095ab634fc6cdc084c9ad
PythonAnywhere developers <developers@resolversystems.com>
2012-05-04 14:08:25 +0000

    Made context processor resilient to missing config params.  by: Giles

--- main/context_processors.py
+++ main/context_processors.py
@@ -2,9 +2,9 @@ from django.conf import settings
 
 def add_blog_settings(context):
     return {
-        'blog_name': settings.BLOG_NAME,
-        'blog_description': settings.BLOG_DESCRIPTION,
-        'blog_author': settings.BLOG_AUTHOR,
-        'blog_author_email': settings.BLOG_AUTHOR_EMAIL,
-        'blog_author_twitter': settings.BLOG_AUTHOR_TWITTER,
+        'blog_name': getattr(settings, "BLOG_NAME", ""),
+        'blog_description': getattr(settings, "BLOG_DESCRIPTION", ""),
+        'blog_author': getattr(settings, "BLOG_AUTHOR", ""),
+        'blog_author_email': getattr(settings, "BLOG_AUTHOR_EMAIL", ""),
+        'blog_author_twitter': getattr(settings, "BLOG_AUTHOR_TWITTER", ""),
     }
