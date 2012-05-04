8331ff734b2514ea07cf07e754824aedfd8b080e
PythonAnywhere developers <developers@resolversystems.com>
2012-05-04 16:55:01 +0000

    Improved list display of posts in admin view.  by: Giles

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from jab.main.models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status',)
+    list_display = ('publication_date', 'title', 'status',)
+
+    ordering = ('-publication_date',)
+
 
 admin.site.register(Post, PostAdmin)
