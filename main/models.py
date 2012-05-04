6a6399f333fac7d4e41ddf618041fd213fcfda12
PythonAnywhere developers <developers@resolversystems.com>
2012-05-04 12:57:22 +0000

    Added post authors.  by: Giles

--- main/models.py
+++ main/models.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Post(models.Model):
 
     published = models.DateTimeField()
+
+    author = models.ForeignKey(User, null=True)
 
     title = models.CharField(max_length=1024)
 
