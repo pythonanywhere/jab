5b3597ead5ddbcd6387a9218be04e1196f2f539c
PythonAnywhere developers <developers@pythonanywhere.com>
2012-05-21 16:19:45 +0000

    First cut at a setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+setup(
+    name="JAB",
+    description="Just A Blog",
+    long_description=open("README").read(),
+    author='PythonAnywhere',
+    author_email='developers@pythonanywhere.com',
+    license="MIT",
+
+    url='http://jab.pythonanywhere.com/',
+    version="0.0.1",
+    download_url="http://jab.pythonanywhere.com/static/downloads/jab-0.1.tar.gz",
+
+    packages=["jab",],
+
+    install_requires=[
+        'south>=0.7.5'
+        'django>=1.3.0'
+    ],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
